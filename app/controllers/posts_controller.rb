7c7de5fee82cd03150b38e02c275601b2a6bf6b6
Jeff Perrin <jeffperrin@gmail.com>
2015-03-26 10:06:44 -0600

    Create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body, :published_at)
+  end
 end
