adeb7d7279795da60b233207e8be0489d46a7ac8
Jeff Perrin <jeffperrin@gmail.com>
2015-03-26 11:38:58 -0600

    Add redcarpet for markdown. Render the post body as markdown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown(blogtext)
+    renderOptions = {hard_wrap: true, filter_html: true}
+    markdownOptions = {autolink: true, no_intra_emphasis: true}
+
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderOptions), markdownOptions)
+    markdown.render(blogtext).html_safe
+  end
 end
